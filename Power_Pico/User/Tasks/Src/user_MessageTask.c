548db44f01dd23bf11837182d81f34129ad9260d
不吃油炸鸡 <1425962791@qq.com>
2025-10-19 11:59:32 +0800

    加入串口消息处理任务

---
+++ Power_Pico/User/Tasks/Src/user_MessageTask.c
@@ -0,0 +1,41 @@
+/* Private includes -----------------------------------------------------------*/
+//includes
+#include "user_TasksInit.h"
+#include "user_MessageTask.h"
+#include "usart.h"
+
+/* Private typedef -----------------------------------------------------------*/
+
+/* Private define ------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+uint8_t test_flag = 0;
+/* Private function prototypes -----------------------------------------------*/
+
+
+/**
+  * @brief  task for check the usart message
+  * @param  argument: Not used
+  * @retval None
+  */
+void MessageTask(void *argument)
+{
+	while(1)
+	{
+    if(uart_receive_flag == 1) {
+
+      // clear the flag
+      uart_receive_flag = 0;
+      // check the received message is "update\r\n" ?
+      if(strcmp((const char *)uart_receive_buf, "update\r\n") == 0) {
+        // send response
+        printf("start update\r\n");
+        // jump to bootloader
+      } else {
+        // do nothing
+      }
+
+    }
+		osDelay(500);
+	}
+}

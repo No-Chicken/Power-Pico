6f80731ceb5cf8632def96ed7310d43350594f41
kingham <1425962791@qq.com>
2026-06-26 19:43:43 +0800

    加入pd fixed

--- software/Power_Pico/User/GUI/ui.c
+++ software/Power_Pico/User/GUI/ui.c
@@ -43,9 +43,9 @@ Page_t pages[] = {
     },
     {
         .init = ui_PDFixedPage_screen_init,
-        .deinit = ui_PPSPage_screen_destroy,
-        .page_obj = &ui_PPSPage,
-        .key_event_handler = ui_pps_page_key_handler,
+        .deinit = ui_PDFixedPage_screen_destroy,
+        .page_obj = &ui_PDFixedPage,
+        .key_event_handler = ui_pdfixed_page_key_handler,
         .name = "PDFixed Page"
     },
     // 可以在这里添加更多页面
